4b71ed7c55c1c160036d5e2f90c9f138ffaf963b
MatrixSeven <hacker.kill07@gmail.com>
2020-12-27 13:44:30 +0800

    update

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,35 +1,50 @@
 Component({
-    data: { 
-      selected: 0,
-      list: [
-        {
-          pagePath: "/pages/index/index",
-          text: "打卡",
-          normal: '/images/tabbar/position.png',
-          active: '/images/tabbar/position-green.png'
-        },
-        {
-          pagePath: "/pages/idcard/idcard",
-          text: "上传",
-          normal: '/images/tabbar/idcard.png',
-          active: '/images/tabbar/idcard-green.png'
-        }
-      ]
-    },
-    methods: {
-      onChange(e) {
-         console.log(e,'e')
-         this.setData({ active: e.detail });
-         wx.switchTab({
-           url: this.data.list[e.detail].pagePath
-         });
+  data: {
+    selected: 0,
+    list: [
+      {
+        pagePath: "/pages/index/index",
+        text: "打卡",
+        normal: '/images/tabbar/position.png',
+        active: '/images/tabbar/position-green.png'
       },
-      init() {
-          const page = getCurrentPages().pop();
-          this.setData({
-         　  active: this.data.list.findIndex(item => item.pagePath === `/${page.route}`)
-          });
-         }
+      {
+        pagePath: "pages/logs/logs",
+        text: "上传_12",
+        normal: '/images/tabbar/idcard.png',
+        active: '/images/tabbar/idcard-green.png'
+      },
+      {
+        pagePath: "pages/logs/logs",
+        text: "上传_12",
+        normal: '/images/tabbar/idcard.png',
+        active: '/images/tabbar/idcard-green.png'
+      },
+      {
+        pagePath: "pages/logs/logs",
+        text: "上传_12",
+        normal: '/images/tabbar/idcard.png',
+        active: '/images/tabbar/idcard-green.png'
+      }, {
+        pagePath: "pages/logs/logs",
+        text: "上传_12",
+        normal: '/images/tabbar/idcard.png',
+        active: '/images/tabbar/idcard-green.png'
       }
-  })
-  
+    ]
+  },
+  methods: {
+    onChange(e) {
+      this.setData({ active: e.detail });
+      wx.switchTab({
+        url: this.data.list[e.detail].pagePath
+      });
+    },
+    init() {
+      const page = getCurrentPages().pop();
+      this.setData({
+        active: this.data.list.findIndex(item => item.pagePath === `/${page.route}`)
+      });
+    }
+  }
+})
