654e3d8f9e063f44ef0d2f495f350aac8a97be41
MatrixSeven <hacker.kill07@gmail.com>
2020-12-30 22:40:12 +0800

    update

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,37 +1,42 @@
 Component({
-  data: {
-    selected: 0,
-    list: [
-      {
-        pagePath: "/pages/index/index",
-        text: "打卡",
-        // normal: '/images/tabbar/position.png',
-        // active: '/images/tabbar/position-green.png'
-      },
-      {
-        pagePath: "/pages/cat/cat",
-        text: "猫咪优惠",
-        // normal: '/images/tabbar/idcard.png',
-        // active: '/images/tabbar/idcard-green.png'
-      },
-      {
-        pagePath: "/pages/subscribe/subscribe",
-        text: "豆瓣开车",
-      },
-    ]
-  },
-  methods: {
-    onChange(e) {
-      this.setData({ active: e.detail });
-      wx.switchTab({
-        url: this.data.list[e.detail].pagePath
-      });
+    data: {
+        // selected: 0,
+        list: [
+            {
+                pagePath: "/pages/index/index",
+                text: "外卖优惠",
+                // normal: '/images/tabbar/position.png',
+                // active: '/images/tabbar/position-green.png'
+            },
+            {
+                pagePath: "/pages/cat/cat",
+                text: "每日猫车",
+                // normal: '/images/tabbar/idcard.png',
+                // active: '/images/tabbar/idcard-green.png'
+            },
+            {
+                pagePath: "/pages/subscribe/subscribe",
+                text: "豆瓣开车",
+            },
+            {
+                pagePath: "/pages/home/home",
+                text: "我的",
+            },
+        ]
     },
-    init() {
-      const page = getCurrentPages().pop();
-      this.setData({
-        active: this.data.list.findIndex(item => item.pagePath === `/${page.route}`)
-      });
+    methods: {
+        onChange(e) {
+            this.setData({active: e.detail});
+            wx.switchTab({
+                url: this.data.list[e.detail].pagePath
+            });
+
+        },
+        init() {
+            const page = getCurrentPages().pop();
+            this.setData({
+                active: this.data.list.findIndex(item => item.pagePath === `/${page.route}`)
+            });
+        }
     }
-  }
 })
