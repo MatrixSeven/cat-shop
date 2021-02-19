2ce36b8f3c1ef774ee6196be7dd16be45134a29d
seven <hacker.kill07@gmail.com>
2021-02-19 17:57:09 +0800

    fix-sub[end-msg]

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
-// const reqUrls = "https://cat-card.52python.cn"
-const reqUrls = "http://192.168.31.237:8443"
+const reqUrls = "https://cat-card.52python.cn"
+// const reqUrls = "http://192.168.31.237:8443"
 
 module.exports = {
     reqUrls: reqUrls
